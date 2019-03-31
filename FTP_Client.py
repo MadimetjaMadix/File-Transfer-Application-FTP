c42ae14f6d65f89833fc848d9e52ebc981badd0f
MadimetjaMadix <1076467@students.wits.ac.za>
2019-03-31 19:08:25 +0200

    Download functionality for both Client and Server

--- FTP_Client.py
+++ FTP_Client.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import time
 import socket
@@ -13,43 +14,58 @@ def initializeFTPConnection(host_name):
 	
 	try:
 		control_socket.connect(host_adr)
+		recv_command(control_socket)
+		print("=====================================================")
 	except:
 		
 		print("Failed to connect to ", host_name )
 		time.sleep(3)
 		return
-		
+	print("				Connected to Server					")	
 	return control_socket
 
-def login(username = '', password = '')
+def login( username, password,control_socket):
 	
-	control_socket = initializeFTPConnection(socket.gethostname())
+	#control_socket = initializeFTPConnection(socket.gethostname())
 	
 	command  = 'USER ' + username + '\r\n' 
-	response = send_command(command)
+	send_command(control_socket, command)
+	response = recv_command(control_socket)
 	
-	ErrorCodes = ['530', '500', '501', '421']
-	if response[0] not in ErrorCodes
+	ErrorCodes = ['530', '500', '501', '421', '403']
+	if response[0] not in ErrorCodes:
 		
 		command  = 'PASS ' + password + '\r\n' 
-		response = send_command(command)
+		send_command(control_socket, command)
+		response = recv_command(control_socket)
 		
-	print('logged in successfuly')
+		if response[0] in ErrorCodes:
+			return False
+			
+		return True
+	else:
+		return False
+		
 
  
 def send_command(control_socket, command):
-	
+	print("Client: ", command)
 	control_socket.send(command.encode())
+
+	
+def recv_command(control_socket):
 	response = control_socket.recv(8192).decode()
 	responseCode, message = response.split(" ", 1)
-	
+	print("Server: " ,response)
 	return responseCode, message
 
 def dataConnection(bufferSize,control_socket):
 	
 	#PASV
 	command  = 'PASV\r\n' 
-	response = send_command(control_socket, command)
+	send_command(control_socket, command)
+	response = recv_command(control_socket)
+	
 	response = response[1]
 	firstBracketIndex = response.find("(")
 	lastBracketIndex  = response.find(")")
@@ -58,27 +74,32 @@ def dataConnection(bufferSize,control_socket):
 	dataPortAddress   = dataPortAddress.split(",")
 	
 	data_host 	 = '.'.join(dataPortAddress[0:4])
-	
+
 	tempDataPort = dataPortAddress[-2:]
-	data_port = (int(tempDataPort[0] * 256) + int(tempDataPort[1]))
+	
+	data_port = int((int(tempDataPort[0]) * 256) + int(tempDataPort[1]))
 	data_port = int(data_port)
 	
 	
 	data_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 	data_socket.connect((data_host,data_port))
-	
+
 	return data_socket
 
-def download_file(bufferSize):
+def download_file(file_Name, data_socket, bufferSize,control_socket):
 	
 	downloadFolderName = "Downloads"
 	
 	if not os.path.exists(downloadFolderName):
 		os.makedirs(downloadFolderName)
 		
+	command  = 'RETR ' + file_Name + '\r\n' 
+	send_command(control_socket, command)
+	response = recv_command(control_socket)
+	
 	if file_Name: 
 		
-		data_socket = dataConnection(bufferSize)
+		#data_socket = dataConnection(bufferSize,control_socket)
 		
 		
 		file_data = data_socket.recv(bufferSize)
@@ -89,3 +110,30 @@ def download_file(bufferSize):
 			file_data = data_socket.recv(bufferSize)
 			
 		data_socket.close()
+		
+def main():
+	
+	bufferSize = 8192
+	
+	host_name = socket.gethostbyname(socket.gethostname())
+	
+	control_socket = initializeFTPConnection(host_name)
+	
+	username = input(str("Enter user name : "))
+	password = input(str("Enter user pass : "))
+	Login_is_valid = login(username, password,control_socket)
+	
+	if Login_is_valid:
+		print("======================================================")
+		data_socket = dataConnection(bufferSize,control_socket)
+		
+		file_Name = "dataBase.txt"
+		
+		download_file(file_Name, data_socket, bufferSize,control_socket)
+		
+	command = 'QUIT\r\n'
+	send_command(control_socket, command)
+	response = recv_command(control_socket)
+	
+if __name__ == '__main__':
+        main()
--- FTP_Server.py
+++ FTP_Server.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 import socket
+import random
 import threading
 
 class FTPServer (threading.Thread):
@@ -14,27 +15,30 @@ class FTPServer (threading.Thread):
 		self.commandConn = connection_socket
 		self.dataConn 	 = None
 		self.client_address = client_address
-		self.cmdBufferSize  = 1024
+		self.cmdBufferSize  = 8192
 		self.dataBufferSize = 8192
 		self.isconnectionActive = True
+		self.ActiveMode = False
 		
 		
 	def run(self):
 		print("Connection from : ", str(self.client_address))
 		reply = "220 Welcome to FTP server\r\n"
+		print(reply)
 		self.commandConn.send(reply.encode())
 		
 		while True:
 			
 			available_commands = ['USER', 'PASS', 'PASV', 'RETR', 'STOR', 'QUIT']
 			
-			client_message = elf.commandConn.recv(self.cmdBufferSize).decode()
+			client_message = self.commandConn.recv(self.cmdBufferSize).decode()
 			command  = client_message[:4].strip()
 			argument = client_message[4:].strip()
 			
 			if not self.isconnectionActive:
 				break
 			
+			# Check command and call respective fn if available
 			if command in available_commands:
 				
 				ftp_command = getattr(self,command)
@@ -45,6 +49,7 @@ class FTPServer (threading.Thread):
 			
 			elif command not in available_commands:
 				reply = "502 command not implemented\r\n"
+				print(reply)
 				self.commandConn.send(reply.encode())
 				
 				
@@ -54,17 +59,20 @@ class FTPServer (threading.Thread):
 		data = file.readlines()
 		file.close()
 		
-		for userInfo in Data:
+		# Check if user is in data-base
+		for userInfo in data:
 			validName, validPass = userInfo.split()
 			if username == validName:
 				self.user = username
 				self.validUser = True
 				reply = "331 password needed\r\n"
+				print(reply)
 				self.commandConn.send(reply.encode())				
 				return
 				
 		if not self.validUser:
 			reply = "530 Invalid User\r\n"
+			print(reply)
 			self.commandConn.send(reply.encode())
 		
 		
@@ -73,37 +81,98 @@ class FTPServer (threading.Thread):
 		data = file.readlines()
 		file.close()
 		
+		# check USER and corrs pass in dataBase
 		if self.validUser:
-			for userInfo in Data:
+			for userInfo in data:
 				validName, validPass = userInfo.split()
 				if self.user == validName and password == validPass:
 					reply = "230 Login successful\r\n"
-					self.commandConn.send(reply.encode())
-					
-			reply = "530 Invalid password\r\n"
-			self.commandConn.send(reply.encode())
+					break
+				else:
+					reply = "500 Invalid password\r\n"
+		self.commandConn.send(reply.encode())
 			
 	def PASV(self):
-		return
+		#passive connection_socket
+		self.ActiveMode = False
+		host_name = socket.gethostbyname(socket.gethostname())
 		
-	def RETR(self):
-		return
+		# calculate a random port number between 12032 and 33023
+		port_num1 = random.randint(47,128)
+		port_num2 = random.randint(0,255)
+		host_port = (port_num1 * 256) + port_num2
+		
+		host_adress = (host_name,host_port)
+		
+		server_address = host_name.split(".")
+		server_address = ','.join(server_address)
+		server_address = "(" + server_address + "," + str(port_num1) +"," + str(port_num2) + ")"
+		
+		# Try to establish a socket connection
+		try:
+			self.dataConn = self.Establish_data_conn(host_adress)
+			reply = "227 Entering Passive Mode " + str(server_address) + "\r\n"
+			print(reply)
+			self.commandConn.send(reply.encode())
+		except:
+			reply = "425 Cannot open Data Connection\r\n"
+			print(reply)
+			self.commandConn.send(reply.encode())
+		
+		
+	def RETR(self, file_name):
+		
+		# check the data connection mode (Active or Passive)
+		if not self.ActiveMode:
+			data_socket, data_address = self.dataConn.accept()
+		
+		# Locate and transfer the file
+		filename = self.cwd + '/' + file_name
+		file = open(filename, 'rb')
+		reading = file.read(self.dataBufferSize)
+		
+		print("Accessing file: ", filename)
+		while reading:
+			print("Reading file ...")
+			if not self.ActiveMode:
+				data_socket.send(reading)
+			else:
+				self.dataConn.send(reading)
+			reading = file.read(self.dataBufferSize)
+			
+		file.close()
+		if not self.ActiveMode:
+			data_socket.close()
+		self.dataConn.close()
+		
+		reply = "266 Data transfer complete\r\n"
+		print(reply)
+		self.commandConn.send(reply.encode())
+		
 		
 	def STOR(self, argument):
-		
 		return
+		
 	def QUIT(self):
+		# Disable the command socket connection
 		self.isconnectionActive = False
 		reply = "221 Goodbye\r\n"
 		self.commandConn.send(reply.encode())
 		self.commandConn.close()
+		
+	def Establish_data_conn(self, host_adress):
+		# Establish a TCP connection
+		data_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		data_socket.bind(host_adress)
+		data_socket.listen(1)
+		return data_socket
 
 
 def main():
 	
 	clientList  = "dataBase.txt"
 	host_name   = socket.gethostbyname(socket.gethostname())
-	host_port 	= 13000
+	host_port 	= 21
 	host_adress = (host_name, host_port)
 	print("Host : ", host_name, " port : ", host_port)
 	
@@ -112,7 +181,6 @@ def main():
 	
 	
 	while True:
-		
 		
 		server_socket.listen(1)
 		connection_socket, client_address = server_socket.accept()
